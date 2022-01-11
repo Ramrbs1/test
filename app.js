9d19e596cd5086baf67b2033e52d29d61ee646ad
Ramrbs1 <86314526+Ramrbs1@users.noreply.github.com>
2022-01-11 11:54:54 +0530

    1
    1

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-server.listen(80);
-console.log("Server running at http://127.0.0.1:80/");
+var http = require("http");
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
